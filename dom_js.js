014e35a3bc572e7ac2a45d1448b81a2eaa70e355
mgunturnn <guntur.nugraha@upi.edu>
2025-10-04 22:45:27 +0700

    add and try to solve study case

---
+++ dom_js.js
@@ -0,0 +1,55 @@
+// APPEND DOM
+// document.title = "Guntur";
+// const body = document.body;
+// body.append("Hello World");
+
+// CREATE & COMBINE DOM
+// const h1 = document.createElement("h1"); // <h1></h1>
+// h1.innerHTML = "Ini adalah H1"; // <h1>Ini adalah H1</h1>
+// body.append(h1); // menambahkan h1 ke dalam body
+
+// const p = document.createElement("p"); // <p></p>
+// p.innerHTML = "Ini adalah paragraf"; // <p>Ini adalah paragraf</p>
+// body.append(p); // menambahkan p ke dalam body
+
+// DATA SELECTOR
+// document.title = "Web Guntur";
+
+// const body = document.body;
+// const button1 = document.getElementById("btn1");
+// const button2 = document.querySelector(".btn2");
+
+// const defaultText = "Klik Saya 1";
+// button1.textContent = defaultText;
+
+// button1.style.border = "none";
+// button1.style.padding = "8px";
+// button1.style.fontSize = "24px";
+// button1.style.background = "blue";
+
+// function gantiWarna() {
+//     button1.style.background = "aqua";
+//     const newText = document.createElement("p");
+//     newText.textContent = "Hello World";
+//     body.append(newText);
+// }
+
+// function ubahNama() {
+//     button1.textContent = "Tombol sudah di klik";
+// }
+
+// function kembaliNama() {
+//     button1.textContent = defaultText;
+// }
+
+// function ubahNama2() {
+//     const newText2 = document.createElement("p");
+//     newText2.textContent = "Guntur";
+//     body.append(newText2);
+//     return newText2;
+// }
+
+// function ubahWarna2() {
+//     const newText2 = ubahNama2();
+//     newText2.style.color = "brown";
+// }
